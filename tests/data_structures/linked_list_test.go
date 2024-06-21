b2d8b49426a1d8301f0f8e4631e2bbfed7357799
evrwaq <evrwaq@gmail.com>
2024-06-21 13:26:44 -0300

    test: ensure LinkedList adds elements

--- tests/data_structures/linked_list_test.go
+++ tests/data_structures/linked_list_test.go
@@ -6,17 +6,42 @@ import (
 )
 
 func TestNewLinkedList(t *testing.T) {
-	linked_list := data_structures.NewLinkedList()
+	linkedList := data_structures.NewLinkedList()
 
 	t.Run("Initial Size", func(t *testing.T) {
-		if linked_list.Size() != 0 {
-			t.Errorf("expected size 0, got %d", linked_list.Size())
+		if linkedList.Size() != 0 {
+			t.Errorf("expected size 0, got %d", linkedList.Size())
 		}
 	})
 
 	t.Run("IsEmpty on new list", func(t *testing.T) {
-		if !linked_list.IsEmpty() {
+		if !linkedList.IsEmpty() {
 			t.Error("expected list to be empty")
+		}
+	})
+}
+
+func TestLinkedListAdd(t *testing.T) {
+	linkedList := data_structures.NewLinkedList()
+
+	t.Run("Add elements", func(t *testing.T) {
+		linkedList.Add(1)
+		if linkedList.Size() != 1 {
+			t.Errorf("expected size 1, got %d", linkedList.Size())
+		}
+
+		linkedList.Add(2)
+		if linkedList.Size() != 2 {
+			t.Errorf("expected size 2, got %d", linkedList.Size())
+		}
+
+		linkedList.Add(3)
+		if linkedList.Size() != 3 {
+			t.Errorf("expected size 3, got %d", linkedList.Size())
+		}
+
+		if linkedList.IsEmpty() {
+			t.Error("expected list to not be empty")
 		}
 	})
 }
