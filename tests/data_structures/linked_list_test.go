40a3d44077c718a92d8d68600d5ba828cf65b78a
evrwaq <evrwaq@gmail.com>
2024-06-21 13:23:15 -0300

    test: ensure LinkedList starts with initial size

---
+++ tests/data_structures/linked_list_test.go
@@ -0,0 +1,16 @@
+package data_structures_test
+
+import (
+	"dsa/src/data_structures"
+	"testing"
+)
+
+func TestNewLinkedList(t *testing.T) {
+	linked_list := data_structures.NewLinkedList()
+
+	t.Run("Initial Size", func(t *testing.T) {
+		if linked_list.Size() != 0 {
+			t.Errorf("expected size 0, got %d", linked_list.Size())
+		}
+	})
+}
