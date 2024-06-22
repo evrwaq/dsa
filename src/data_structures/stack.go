64018226fe4f6052a931f37d494684bbb83acd83
evrwaq <evrwaq@gmail.com>
2024-06-22 00:21:53 -0300

    feat: ensure Stack starts empty

---
+++ src/data_structures/stack.go
@@ -0,0 +1,17 @@
+package data_structures
+
+type Stack struct {
+	elements []interface{}
+}
+
+func NewStack() *Stack {
+	return &Stack{elements: []interface{}{}}
+}
+
+func (stack *Stack) Size() int {
+	return len(stack.elements)
+}
+
+func (stack *Stack) IsEmpty() bool {
+	return len(stack.elements) == 0
+}
