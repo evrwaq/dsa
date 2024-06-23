453864234115a26a3c68d192b7824093aade13d6
evrwaq <evrwaq@gmail.com>
2024-06-22 21:12:52 -0300

    feat: ensure Deque starts with empty size

---
+++ src/data_structures/deque.go
@@ -0,0 +1,17 @@
+package data_structures
+
+type Deque struct {
+	elements []interface{}
+}
+
+func NewDeque() *Deque {
+	return &Deque{elements: []interface{}{}}
+}
+
+func (deque *Deque) Size() int {
+	return len(deque.elements)
+}
+
+func (deque *Deque) IsEmpty() bool {
+	return len(deque.elements) == 0
+}
