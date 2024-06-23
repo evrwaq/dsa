31e78e1797aa195ac3573b612aa9360395db7651
evrwaq <evrwaq@gmail.com>
2024-06-22 21:18:46 -0300

    testt: ensure Deque remove front elements

--- tests/data_structures/deque_test.go
+++ tests/data_structures/deque_test.go
@@ -70,3 +70,45 @@ func TestDequeAddBack(t *testing.T) {
 		}
 	})
 }
+
+func TestDequeRemoveFront(t *testing.T) {
+	deque := data_structures.NewDeque()
+	deque.AddFront(1)
+	deque.AddFront(2)
+	deque.AddFront(3)
+
+	t.Run("RemoveFront elements", func(t *testing.T) {
+		value, error := deque.RemoveFront()
+		if error != nil {
+			t.Errorf("unexpected error: %v", error)
+		}
+		if value != 3 {
+			t.Errorf("expected value 3, got %v", value)
+		}
+		if deque.Size() != 2 {
+			t.Errorf("expected size 2, got %deque", deque.Size())
+		}
+
+		value, error = deque.RemoveFront()
+		if error != nil {
+			t.Errorf("unexpected error: %v", error)
+		}
+		if value != 2 {
+			t.Errorf("expected value 2, got %v", value)
+		}
+		if deque.Size() != 1 {
+			t.Errorf("expected size 1, got %deque", deque.Size())
+		}
+
+		value, error = deque.RemoveFront()
+		if error != nil {
+			t.Errorf("unexpected error: %v", error)
+		}
+		if value != 1 {
+			t.Errorf("expected value 1, got %v", value)
+		}
+		if deque.Size() != 0 {
+			t.Errorf("expected size 0, got %deque", deque.Size())
+		}
+	})
+}
