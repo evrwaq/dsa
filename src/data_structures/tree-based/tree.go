d589404ea8793991daf877a8a12731f99cb55eec
evrwaq <evrwaq@gmail.com>
2024-07-03 23:36:37 -0300

    feat: ensure Tree inserts with correct childs

--- src/data_structures/tree-based/tree.go
+++ src/data_structures/tree-based/tree.go
@@ -16,3 +16,19 @@ func NewTree() *Tree {
 func (tree *Tree) IsEmpty() bool {
 	return tree.Root == nil
 }
+
+func (tree *Tree) Insert(value interface{}) {
+	tree.Root = insert(tree.Root, value)
+}
+
+func insert(node *TreeNode, value interface{}) *TreeNode {
+	if node == nil {
+		return &TreeNode{Value: value}
+	}
+	if value.(int) < node.Value.(int) {
+		node.Left = insert(node.Left, value)
+	} else {
+		node.Right = insert(node.Right, value)
+	}
+	return node
+}
