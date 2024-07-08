21c92ea6e1b36446f107f8accb6e004aa0105780
evrwaq <evrwaq@gmail.com>
2024-07-07 22:30:10 -0300

    feat: ensure BinaryTree inserts a value

--- src/data_structures/tree-based/binary_tree.go
+++ src/data_structures/tree-based/binary_tree.go
@@ -16,3 +16,19 @@ func NewBinaryTree() *BinaryTree {
 func (tree *BinaryTree) IsEmpty() bool {
 	return tree.Root == nil
 }
+
+func (tree *BinaryTree) Insert(value interface{}) {
+	tree.Root = insertBinary(tree.Root, value)
+}
+
+func insertBinary(node *BinaryTreeNode, value interface{}) *BinaryTreeNode {
+	if node == nil {
+		return &BinaryTreeNode{Value: value}
+	}
+	if value.(int) < node.Value.(int) {
+		node.Left = insertBinary(node.Left, value)
+	} else {
+		node.Right = insertBinary(node.Right, value)
+	}
+	return node
+}
