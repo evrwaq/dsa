b5152fa73e79e6d923c62f8ade661595ebe03277
evrwaq <evrwaq@gmail.com>
2024-07-07 22:26:20 -0300

    test: ensure BinaryTree starts correct

---
+++ tests/data_structures/tree-based/binary_tree_test.go
@@ -0,0 +1,16 @@
+package data_structures_test
+
+import (
+	data_structures "dsa/src/data_structures/tree-based"
+	"testing"
+)
+
+func TestNewBinaryTree(t *testing.T) {
+	tree := data_structures.NewBinaryTree()
+
+	if tree == nil {
+		t.Error("expected tree to be non-nil")
+	} else if tree.Root != nil {
+		t.Error("expected root to be nil for a new tree")
+	}
+}
