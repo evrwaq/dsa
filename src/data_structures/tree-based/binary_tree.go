19f704e4b5325e12fd7f9db24b7390cbafc28c1e
evrwaq <evrwaq@gmail.com>
2024-07-07 22:33:48 -0300

    feat: ensure BinaryTree removes a value

--- src/data_structures/tree-based/binary_tree.go
+++ src/data_structures/tree-based/binary_tree.go
@@ -1,5 +1,7 @@
 package data_structures
 
+import "errors"
+
 type BinaryTreeNode struct {
 	Value       interface{}
 	Left, Right *BinaryTreeNode
@@ -48,4 +50,43 @@ func searchBinary(node *BinaryTreeNode, value interface{}) (bool, error) {
 	} else {
 		return searchBinary(node.Right, value)
 	}
+}
+
+func (tree *BinaryTree) Remove(value interface{}) error {
+	var err error
+	tree.Root, err = removeBinary(tree.Root, value)
+	return err
+}
+
+func removeBinary(node *BinaryTreeNode, value interface{}) (*BinaryTreeNode, error) {
+	if node == nil {
+		return nil, errors.New("value not found in the tree")
+	}
+	if value.(int) < node.Value.(int) {
+		var err error
+		node.Left, err = removeBinary(node.Left, value)
+		return node, err
+	} else if value.(int) > node.Value.(int) {
+		var err error
+		node.Right, err = removeBinary(node.Right, value)
+		return node, err
+	} else {
+		if node.Left == nil {
+			return node.Right, nil
+		} else if node.Right == nil {
+			return node.Left, nil
+		}
+		minLargerNode := findMinBinary(node.Right)
+		node.Value = minLargerNode.Value
+		var err error
+		node.Right, err = removeBinary(node.Right, minLargerNode.Value)
+		return node, err
+	}
+}
+
+func findMinBinary(node *BinaryTreeNode) *BinaryTreeNode {
+	for node.Left != nil {
+		node = node.Left
+	}
+	return node
 }
