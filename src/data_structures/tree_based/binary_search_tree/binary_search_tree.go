e9c9b9d7d1853bceca2ff6b0294e079c16d63354
evrwaq <evrwaq@gmail.com>
2024-07-13 17:04:24 -0300

    feat: ensure BinarySearchTree inserts on success

--- src/data_structures/tree_based/binary_search_tree/binary_search_tree.go
+++ src/data_structures/tree_based/binary_search_tree/binary_search_tree.go
@@ -16,3 +16,36 @@ func NewBinaryTree() *BinaryTree {
 func (tree *BinaryTree) IsEmpty() bool {
 	return tree.Root == nil
 }
+
+func (tree *BinaryTree) Insert(value int) {
+	tree.Root = insert(tree.Root, value)
+}
+
+func insert(node *TreeNode, value int) *TreeNode {
+	if node == nil {
+		return &TreeNode{Value: value}
+	}
+	if value < node.Value {
+		node.Left = insert(node.Left, value)
+	} else {
+		node.Right = insert(node.Right, value)
+	}
+	return node
+}
+
+func (tree *BinaryTree) Search(value int) (bool, error) {
+	return search(tree.Root, value)
+}
+
+func search(node *TreeNode, value int) (bool, error) {
+	if node == nil {
+		return false, nil
+	}
+	if value == node.Value {
+		return true, nil
+	} else if value < node.Value {
+		return search(node.Left, value)
+	} else {
+		return search(node.Right, value)
+	}
+}
