c825108d9a3e8a18081059d9dd7bfe9fd0710f49
evrwaq <evrwaq@gmail.com>
2024-07-20 15:25:44 -0300

    feat: ensure Trie inserts correct values

---
+++ src/data_structures/tree_based/trie/trie.go
@@ -0,0 +1,34 @@
+package data_structures
+
+type TrieNode struct {
+	Children map[rune]*TrieNode
+	IsEnd    bool
+}
+
+type Trie struct {
+	Root *TrieNode
+}
+
+func NewTrieNode() *TrieNode {
+	return &TrieNode{
+		Children: make(map[rune]*TrieNode),
+		IsEnd:    false,
+	}
+}
+
+func NewTrie() *Trie {
+	return &Trie{
+		Root: NewTrieNode(),
+	}
+}
+
+func (t *Trie) Insert(word string) {
+	node := t.Root
+	for _, char := range word {
+		if _, exists := node.Children[char]; !exists {
+			node.Children[char] = NewTrieNode()
+		}
+		node = node.Children[char]
+	}
+	node.IsEnd = true
+}
