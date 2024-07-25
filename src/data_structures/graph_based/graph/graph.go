201472f6e5f53b42a5d9b26e02d9c5d61dae8983
evrwaq <evrwaq@gmail.com>
2024-07-24 22:43:28 -0300

    feat: ensure Graph starts with no vertices

---
+++ src/data_structures/graph_based/graph/graph.go
@@ -0,0 +1,21 @@
+package data_structures
+
+type Graph struct {
+	vertices map[string]struct{}
+	edges    map[string]map[string]struct{}
+}
+
+func NewGraph() *Graph {
+	return &Graph{
+		vertices: make(map[string]struct{}),
+		edges:    make(map[string]map[string]struct{}),
+	}
+}
+
+func (g *Graph) GetVertices() []string {
+	vertices := make([]string, 0, len(g.vertices))
+	for vertex := range g.vertices {
+		vertices = append(vertices, vertex)
+	}
+	return vertices
+}
