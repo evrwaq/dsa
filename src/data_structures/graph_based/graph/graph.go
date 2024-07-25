b81c3e0de8b40ca20600eb80714a190b396da366
evrwaq <evrwaq@gmail.com>
2024-07-24 22:44:55 -0300

    feat: ensure Graph adds a new vertex

--- src/data_structures/graph_based/graph/graph.go
+++ src/data_structures/graph_based/graph/graph.go
@@ -12,6 +12,13 @@ func NewGraph() *Graph {
 	}
 }
 
+func (g *Graph) AddVertex(vertex string) {
+	if _, exists := g.vertices[vertex]; !exists {
+		g.vertices[vertex] = struct{}{}
+		g.edges[vertex] = make(map[string]struct{})
+	}
+}
+
 func (g *Graph) GetVertices() []string {
 	vertices := make([]string, 0, len(g.vertices))
 	for vertex := range g.vertices {
