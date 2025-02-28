c10d8232a6957800a70e0377bbabf238eb29735f
Evrwaq <evrwaq@gmail.com>
2025-02-27 21:01:15 -0300

    test: add example hello

---
+++ tests/example/hello_test.go
@@ -0,0 +1,16 @@
+package example_test
+
+import (
+	"testing"
+
+	"github.com/evrwaq/dsa/internal/example"
+)
+
+func TestHello(t *testing.T) {
+	expected := "Hello, World!"
+	result := example.Hello()
+
+	if result != expected {
+		t.Errorf("Expected: %s, Result: %s", expected, result)
+	}
+}
